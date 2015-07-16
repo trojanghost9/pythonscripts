e232f3446bcdc3fb69387079b52dd515400ca657
rxduran <rxduran@godaddy.com>
2015-07-16 15:28:55 -0700

    url grabber now kicks ass and adding git ignore

--- url_grabber.py
+++ url_grabber.py
@@ -1,41 +1,56 @@
-# this script can be used to grab the url's for some google search result
+import os
+import sys
+import time
+from urllib import FancyURLopener
+import urllib2
+import simplejson
 
-import sys # Used to add the BeautifulSoup folder the import path
-import urllib2 # Used to read the html document
-import re
+# Define search term
+searchTerm = "pun memes"
+
+# Replace spaces ' ' in search term for '%20' in order to comply with request
+searchTerm = searchTerm.replace(' ', '%20')
+
+
+# Start FancyURLopener with defined version
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11)' \
+              ' Gecko/20071127 Firefox/2.0.0.11'
+
+myopener = MyOpener()
+
+
+def capture():
+    """
+    Uses defined searchterm to grab related image urls from google.
+    """
+
+    # Set count to 0
+    count = 0
+
+    for start in range(0, 15):
+        # the "start" variable will be used to iterate through 15 pages
+        url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
+               'v=1.0&q='+searchTerm+'&start='+str(start*4)+'&userip=MyIP')
+        # printing out the url for testing
+        # print url
+        request = urllib2.Request(url, None, {'Referer': 'testing'})
+        response = urllib2.urlopen(request)
+
+        # Get results using JSON
+        results = simplejson.load(response)
+        data = results['responseData']
+        datainfo = data['results']
+
+        # Iterate for each result and get unescaped url
+        for myUrl in datainfo:
+            count += 1
+            print myUrl['unescapedUrl']
+            # use if you want to save a copy of the image
+            # myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
+
+        # Sleep for one second to prevent IP blocking from Google
+        time.sleep(1)
 
 if __name__ == "__main__":
-    ### Import Beautiful Soup
-    ### Here, I have the BeautifulSoup folder in the level of this Python script
-    ### So I need to tell Python where to look.
-    sys.path.append("./BeautifulSoup")
-    from BeautifulSoup import BeautifulSoup
-
-    ### Create opener with Google-friendly user agent
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-
-    ### Open page & generate soup
-    ### the "start" variable will be used to iterate through 10 pages.
-    for start in range(0,10):
-        # url = "http://www.google.com/search?q=site:stackoverflow.com&start=" + str(start*10)
-        # url must be set to the search
-        url = "https://www.google.com/search?q=pun+memes&tbm=isch&start=" + str(start*10)
-	page = opener.open(url)
-        # print page
-        soup = BeautifulSoup(page)
-        # print soup
-
-        ### Parse and find
-        ### Looks like google contains URLs in <cite> tags.
-        ### So for each cite tag on each page (10), print its contents (url)
-        list = []
-        for text in soup.findAll('img'):
-            pattern = r'src="(http\S{1,})"'
-	    link = re.findall(pattern, str(text))
-	    # this needs work, prints out in ugly format and leaves brackets
-            # may also wanna look at using this https://developers.google.com/custom-search/json-api/v1/overview?csw=1
-            print link
-	    list += link
-
-    # print list
+    capture()
