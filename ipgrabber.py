a069610ab2fa05efd77b110f68dceb109e80b497
rxduran <rxduran@godaddy.com>
2015-09-11 10:32:44 -0700

    added comment and sort command help

--- ipgrabber.py
+++ ipgrabber.py
@@ -3,6 +3,13 @@ import re
 pattern = re.compile(r"((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])[ (\[]?(\.|dot)[ )\]]?)"
                       r"{3}[0-9]{1,3})")
 
+"""
+you can write these results out to a file (ips.txt)
+for now i can then use the following command to sort the results by the biggest talkers: 
+sort -t " " -k 3 -n ips.txt > orderedfile.txt
+"""
+
+
 with open('list.txt', 'r') as f:
     ipdic = {}
     for l in f:
