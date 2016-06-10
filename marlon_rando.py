34ea01ec72d37fcdc08ff7bc9c2e9e80f1d900b1
ray <trojanghost9@gmail.com>
2016-06-10 16:27:31 -0700

    because reasons

---
+++ marlon_rando.py
@@ -0,0 +1,17 @@
+# Privacy is not something that I'm merely entitled to, it's an absolute prerequisite. -MB
+
+import random, string
+from time import sleep
+
+def id_generator(size=6, chars=string.letters + string.punctuation + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+if __name__ == '__main__':
+    x = 0
+    while x < 180:
+        print id_generator(134)
+        x += 1
+        sleep(1)
+    else:
+        print 'The Cuttlefish has cuttled you!'
