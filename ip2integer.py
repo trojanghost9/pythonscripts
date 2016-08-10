04e4b0a086524074e20ded61385efb554683e896
raduran <raduran@paypal.com>
2016-08-10 14:32:02 -0700

    added error handling

--- ip2integer.py
+++ ip2integer.py
@@ -1,8 +1,9 @@
-# used to convert IPv4 to an integer that browsers will resolve as an address
+from IPy import IP
 
 
 def convert():
     """
+    used to convert IPv4 to an integer that browsers will resolve as a web address
     ip = the IPv4 address that is entered by the user
     ip_split = the IP split into octets
     first = first octet
@@ -13,16 +14,20 @@ def convert():
     :return: the IPv4 address as an integer address
     """
     ip = raw_input("enter an IPv4 IP address: ")
-    ip_split = ip.split(".")
+    try:
+        IP(ip)
+        ip_split = ip.split(".")
 
-    first = ip_split[0]
-    second = ip_split[1]
-    third = ip_split[2]
-    forth = ip_split[3]
+        first = ip_split[0]
+        second = ip_split[1]
+        third = ip_split[2]
+        forth = ip_split[3]
 
-    integer_address = (int(first) * (256**3)) + (int(second) * (256**2)) + (int(third) * 256) + int(forth)
+        integer_address = (int(first) * (256**3)) + (int(second) * (256**2)) + (int(third) * 256) + int(forth)
 
-    print "http://" + str(integer_address)
+        print "http://" + str(integer_address)
+    except IndexError:
+        print ("You need to enter a valid IPv4 address. i.e. 127.0.0.1")
 
 if __name__ == "__main__":
     convert()
