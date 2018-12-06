cd2b5e943a7137eb380bd9fda7dcdcf77f8f1a85
trojanghost <trojanghost9@gmail.com>
2018-12-06 05:23:53 +0000

    ported to python3

--- marlon_rando.py
+++ marlon_rando.py
@@ -5,15 +5,15 @@ import string
 from time import sleep
 
 
-def id_generator(size=6, chars=string.letters + string.punctuation + string.digits):
+def id_generator(size=6, chars=string.ascii_letters + string.punctuation + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
 if __name__ == '__main__':
     x = 0
     while x < 180:
-        print id_generator(134)
+        print(id_generator(134))
         x += 1
         sleep(1)
     else:
-        print 'The Cuttlefish has cuttled you!'
+        print('The Cuttlefish has cuttled you!')
